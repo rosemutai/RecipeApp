731bceead3740992f43db0ba03d6f9e7f5ee96bc
rosemutai <chepngetichrose2030@gmail.com>
2023-06-01 18:56:09 +0300

    Add Login Page

---
+++ src/routes/Login.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import { account } from '../appwrite'
+
+const Login = () => {
+
+  const [email, SetEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const promise = account.createEmailSession(email, password)
+    promise.then((response) => {
+      console.log(response)
+      // router.push('/')
+    }, (error) => {
+      console.log(error);
+    })
+  }
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="email">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => SetEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="password">
+          <label htmlFor="password">Email</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button type="submit">Login</button>
+        <a href="#">Sign Up</a>
+      </form>
+    </div>
+  );
+}
+
+export default Login
